0869497bebd9bf50e616877972804dd274083744
juanquij0te <jonathan.wyrick@nist.gov>
2020-04-08 16:58:49 -0400

    added install.bat

---
+++ ABDController/install.bat
@@ -0,0 +1,2 @@
+javac -cp lib/* -d bin src/bot/*.java src/com/*.java src/controllers/lithoRasters/*.java src/controllers/*.java src/gds/*.java src/gui/*.java src/main/*.java
+copy src\com_MatrixInterface.h bin\com_MatrixInterface.h
