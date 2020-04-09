9d24c6a858b8b2b1c10f6897fff2333273dba2ae
Jonathan Wyrick <jonathan.wyrick@nist.gov>
2020-04-09 18:41:23 -0400

    Update install.bat

--- ABDController/install.bat
+++ ABDController/install.bat
@@ -1,2 +1,4 @@
 javac -cp lib/* -d bin src/bot/*.java src/com/*.java src/controllers/lithoRasters/*.java src/controllers/*.java src/gds/*.java src/gui/*.java src/main/*.java
+javah com.MatrixInterface
 copy src\com_MatrixInterface.h bin\com_MatrixInterface.h
+copy src\com_MatrixInterface.h ..\ABDController_C_Code\vc\com_MatrixInterface\com_MatrixInterface\com_MatrixInterface.h
